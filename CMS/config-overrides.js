b17aba64ab4b20935f8a5040b079f8f18adf4be8
LiLixikun <460373604@qq.com>
2020-03-27 13:46:12 +0800

    Add CMS

---
+++ CMS/config-overrides.js
@@ -0,0 +1,20 @@
+const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const darkThemeVars = require('antd/dist/dark-theme');
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            'hack': `true;@import "${require.resolve('antd/lib/style/color/colorPalette.less')}";`,
+            ...darkThemeVars,
+            '@primary-color': '#1DA57A'
+        },
+    }),
+    addWebpackPlugin(new AntdDayjsWebpackPlugin())
+);
