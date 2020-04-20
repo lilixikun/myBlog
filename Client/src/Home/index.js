d88765860863baa9362927ea561642ab1027ba16
LiLixikun <460373604@qq.com>
2020-04-20 18:56:35 +0800

    Update 博客前段修改部分样式

--- Client/src/Home/index.js
+++ Client/src/Home/index.js
@@ -1,10 +1,15 @@
 import React, { PureComponent, Fragment } from 'react'
 import { connect } from "react-redux";
+import { getRandomColor } from '../utils'
 import './index.less'
+
+
+const tags = ["前段", "Sql", "Vue", "JAVA", "测试", "Python"]
 
 class Home extends PureComponent {
 
     render() {
+
         return (
             <Fragment>
                 <div className="content">
@@ -12,48 +17,73 @@ class Home extends PureComponent {
                         Left
                     </aside>
                     <aside className='aside-right'>
-                        {/* <div>
-                            <img src="https://pics6.baidu.com/feed/d058ccbf6c81800a0baf2c6fce53b9fc838b47f1.jpeg?token=65fa1a02296996f59441cff18912e4c2" />
-                        </div>
-                        <div>
-                            <img src="https://pics6.baidu.com/feed/d058ccbf6c81800a0baf2c6fce53b9fc838b47f1.jpeg?token=65fa1a02296996f59441cff18912e4c2" />
-                        </div> */}
-
                         <div className="xk-tags">
                             <div className='hot-tas'>热门标签</div>
                             <section className='tag-content'>
-                                <div className="tag">
-                                    <div className='sort'>前段</div>
-                                    <div>10篇文章</div>
-                                </div>
-                                <div className="tag">
-
-                                </div>
-                                <div className="tag">
-
-                                </div>
-                                <div className="tag">
-
-                                </div>
-                                <div className="tag">
-
-                                </div>
-                                <div className="tag">
-
-                                </div>
-                                <a className='allTags'>查看全部</a>
+                                {tags.map((item, index) => (
+                                    <div className="tag" style={{ background: getRandomColor() }} key={index}>{item}</div>
+                                ))}
                             </section>
                         </div>
 
-                        <div className="xk-tags" style={{ marginTop: 50 }}>
+                        <div className="xk-tags" style={{ marginTop: 20 }}>
                             <div className='hot-tas'>推荐文章</div>
                             <section className='tag-content'>
                                 <div className='hot-article'>
-                                    <img src="https://pics6.baidu.com/feed/d058ccbf6c81800a0baf2c6fce53b9fc838b47f1.jpeg?token=65fa1a02296996f59441cff18912e4c2" />
+                                    <div className='img-wrapper'>
+                                        <img src="https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1587982384&t=da2294a846663c27e8b3b0bd00cf6405" />
+                                    </div>                                    <div>
+                                        <p className='title'>CSS3中常见的单位及总结</p>
+                                        <p className='time'>2020-03-29</p>
+                                    </div>
+                                    <div className="sort">1</div>
+                                </div>
+                                <div className='hot-article'>
+                                    <div className='img-wrapper'>
+                                        <img src="https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1587982384&t=da2294a846663c27e8b3b0bd00cf6405" />
+                                    </div>                                    <div>
+                                        <p className='title'>CSS3中常见的单位及总结</p>
+                                        <p className='time'>2020-03-29</p>
+                                    </div>
+                                    <div className="sort">2</div>
+                                </div>
+                                <div className='hot-article'>
+                                    <div className='img-wrapper'>
+                                        <img src="https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1587982384&t=da2294a846663c27e8b3b0bd00cf6405" />
+                                    </div>
                                     <div>
                                         <p className='title'>CSS3中常见的单位及总结</p>
                                         <p className='time'>2020-03-29</p>
                                     </div>
+                                    <div className="sort">3</div>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>4</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>5</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>6</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>7</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>8</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>9</span>
+                                    <p className='title'>非官方个风格非官方</p>
+                                </div>
+                                <div className='sort-content'>
+                                    <span>10</span>
+                                    <p className='title'>非官方个风格非官方</p>
                                 </div>
                             </section>
                         </div>
--- Client/src/utils/index.js
+++ Client/src/utils/index.js
@@ -6,3 +6,15 @@ export const getActivePath = (url) => {
     const path = url.split("?")[1]
     return path
 }
+
+// 随机生成颜色RGB  核心点 ：（0,0,0)   rgb  每一组的数字取值范围是  0~255 
+export const getRandomColor = () => {
+    // 需要随机生成 0~255 之间的整数
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var c1 = getRandom(0, 255);
+    var c2 = getRandom(0, 255);
+    var c3 = getRandom(0, 255);
+    return 'rgba(' + c1 + ',' + c2 + ',' + c3 + ',' + 0.8 + ')'
+}
